b602bbc37a92581fbe0762be18782382ce789d28
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-03 17:03:10 +0800

    test

--- src/ion/IonKt.kt
+++ src/ion/IonKt.kt
@@ -6,10 +6,15 @@ import mindustry.game.EventType.*
 import mindustry.mod.*
 import mindustry.ui.dialogs.*
 
+import ion.ui.*
+
 class IonKt : Mod(){
     
     init{
         Log.infoTag("IKTLoad", "Loaded main IonKt class.")
+        Events.on(ClientLoadEvent::class.java){
+            ISettings.load()
+        }
     }
     
     override fun loadContent(){
---
+++ src/ion/ui/ISettings.kt
@@ -0,0 +1,17 @@
+package ion.ui
+
+import arc.Core.app
+import mindustry.Vars.ui
+import mindustry.gen.Icon
+
+object ISettings{
+    fun load(){
+        ui.settings.addCategory("Ion", Icon.right).apply{
+            textPref("Input", "..."){
+                when(it){
+                    "horny" -> app.exit()
+                }
+            }
+        }
+    }
+}
