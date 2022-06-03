3f9d65e4192af23d5700447f981bd0e4f8a1eaaa
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-03 22:44:40 +0800

    IonItems fix 2

--- src/ion/content/IonItems.kt
+++ src/ion/content/IonItems.kt
@@ -11,7 +11,7 @@ object IonItems{
         zinc = Item("zinc", Color.valueOf("9c9fa6")).apply{
             explosiveness = 0.25f
             hardness = 4
-            cost = 2
+            cost = 2f
         }
     }
 }
