e20392ddd2834f59088d628bbace3ca65d3c7afe
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-03 16:38:30 +0800

    set

---
+++ src/ion/IonKt.kt
@@ -0,0 +1,17 @@
+package ion
+
+import arc.*
+import arc.util.*
+import mindustry.game.EventType.*
+import mindustry.mod.*
+import mindustry.ui.dialogs.*
+
+class IonKt : Mod(){
+    
+    init{
+        Log.infoTag("IKTLoad", "Loaded main IonKt class.")
+    }
+    
+    override fun loadContent(){
+    }
+}
