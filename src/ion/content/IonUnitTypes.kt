2db041124c700cb0bc34d97b9428a256ad80ed51
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-03 23:15:28 +0800

    Orion port

---
+++ src/ion/content/IonUnitTypes.kt
@@ -0,0 +1,52 @@
+package ion.content
+
+import arc.*
+import arc.graphics.*
+import mindustry.gen.*
+import mindustry.type.*
+import mindustry.game.*
+import mindustry.content.*
+import mindustry.entities.bullet.*
+import mindustry.entities.pattern.*
+
+import ion.defs.*
+import ion.content.*
+import ion.entities.bullet.*
+
+object IonUnitTypes{
+    
+    lateinit var orion: UnitType
+    lateinit var xender: UnitType
+    lateinit var astro: UnitType
+    lateinit var geometry: UnitType
+    lateinit var xeus: UnitType
+    
+    fun load(){
+        orion = UnitType("orion").apply{
+            flying = true
+            health = 290f
+            speed = 3.62f
+            accel = 0.15f
+            drag = 0.05f
+            engineOffset = 7f
+            hitSize = 13f
+            
+            constructor = UnitEntity::create
+            
+            
+            weapons.add(Weapon("orion-arc").apply{
+                x = 0f
+                y = 0f
+                reload = 40f
+                mirror = false
+                shootSound = Sounds.spark
+                shoot.shots = 2
+                bullet = LightningBulletType().apply{
+                    damage = 14f
+                    lightningColor = IColor.energy
+                    lightningLength = 19
+                }
+            })
+        }
+    }
+}
