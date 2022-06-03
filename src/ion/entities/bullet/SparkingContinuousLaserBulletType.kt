8038f6726fdecb48ac7411e929d289ea66427e0a
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-03 22:53:19 +0800

    SparkingContinuousLaserBulletType port

---
+++ src/ion/entities/bullet/SparkingContinuousLaserBulletType.kt
@@ -0,0 +1,27 @@
+package ion.entities.bullet
+
+import arc.math.*
+import arc.graphics.*
+import mindustry.gen.*
+import mindustry.entities.*
+import mindustry.entities.bullet.*
+
+open class SparkingContinuousLaserBulletType : ContinuousLaserBulletType{
+    
+    var lightningSpawnChance = 0.3f;
+    var lightningDamage = 1f;
+    var lightningLength = 24;
+    var lightningColor = Color.white;
+    
+    constructor(lSpawnChance: Float) : super(){
+        lightningSpawnChance = lSpawnChance;
+    }
+    
+    override fun update(b: Bullet){
+        super.update(b)
+        
+        if(Mathf.chance(lightningSpawnChance)){
+            Lightning.create(b.team, lightningColor, lightningDamage, b.x, b.y, b.rotation(), lightningLength)
+        }
+    }
+}
