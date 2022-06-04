a41cd592f4ed6d015da7da130ef7039b02180e25
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-04 12:35:45 +0800

    complete port

--- src/ion/ui/ISettings.kt
+++ src/ion/ui/ISettings.kt
@@ -1,7 +1,8 @@
 package ion.ui
 
 import arc.Core.app
-import mindustry.Vars.ui
+import arc.util.*
+import mindustry.Vars.*
 import mindustry.gen.Icon
 
 object ISettings{
@@ -11,8 +12,26 @@ object ISettings{
                 when(it){
                     "horny" -> app.exit()
                     
-                    "plus one" -> ui.showInfo("[red]angry cheesy-chan noises[]")
-                    "pet the cheesy-chan" -> ui.showConfirm("Question", "Have you gotten [accent]consent[]?"){ ui.showInfo("the cheesy-chan has been pet.") }
+                    "amogus" -> ui.showInfo("sugosmu garitesu")
+                    
+                    "seq" -> {
+                        var waitLength = Mathf.random(600f)
+                        ui.loadfrag.show("""
+                            seqs uwu
+                            wait ${waitLength / 60f} seconds...
+                        """.trimIndent())
+                        Time.runTask(waitLength) { ui.loadfrag.hide() }
+                    }
+                    
+                    "screw you 192.168.1.34" -> Sounds.wind3.play(Mathf.random(200f))
+                    
+                    "how do i pronounce mindustry" -> ui.showInfo(tree["texts/methods.txt"].readString())
+                    
+                    "plus one" -> Log.warn("angry cheesy-chan noises")
+                    
+                    "tantos" -> Threads.throwAppException(RuntimeException(tree["texts/tantos.txt"].readString()))
+                    
+                    "pet the cheesy-chan" -> ui.showConfirm("Question","have you gotten [accent]consent[]?") { ui.showInfo("the cheesy-chan has been pet.") }
                 }
             }
         }
