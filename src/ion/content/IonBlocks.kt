bc183115bbb2de939604e956f370905fd06c3d6b
xjammuux <64030380+xjamiex@users.noreply.github.com>
2022-06-04 19:08:54 -0700

    huh sprite update

--- src/ion/content/IonBlocks.kt
+++ src/ion/content/IonBlocks.kt
@@ -7,11 +7,13 @@ import mindustry.world.blocks.units.*
 import mindustry.world.blocks.units.UnitFactory.UnitPlan
 
 import ion.content.IonItems
+import mindustry.graphics.Layer.plans
 
 object IonBlocks{
 
     lateinit var geoEnergeticAirFactory: Block
     lateinit var gonicReconstructor: Block
+    lateinit var alephReconstructor: Block
     
     fun load(){
         geoEnergeticAirFactory = UnitFactory("geo-energetic-air-factory").apply{
@@ -29,7 +31,7 @@ object IonBlocks{
             consumePower(2f)
             plans.addAll(
                 UnitPlan(
-                    IonUnitTypes.orion, 20f * 60, ItemStack.with(
+                    IonUnitTypes.orion, 12f * 60, ItemStack.with(
                         Items.silicon, 10,
                         Items.titanium, 25,
                         IonItems.zinc, 5
@@ -53,13 +55,39 @@ object IonBlocks{
             consumePower(2.4f)
             consumeItems(
                 *ItemStack.with(
-                    Items.lead, 30,
-                    Items.titanium, 25,
+                    Items.graphite, 40,
                     Items.silicon, 45,
                     IonItems.zinc, 20
                 )
             )
+            constructTime = 15 * 60f
             upgrades.add(arrayOf(IonUnitTypes.orion, IonUnitTypes.xender))
+        }
+
+        alephReconstructor = Reconstructor("aleph-reconstructor").apply {
+            requirements(
+                Category.units, ItemStack.with(
+                    Items.lead, 520,
+                    Items.metaglass, 100,
+                    Items.silicon, 230,
+                    Items.titanium, 430,
+                    Items.thorium, 240,
+                    IonItems.zinc, 60
+                )
+            )
+            health = 840
+            size = 5
+            consumePower(3.2f)
+            consumeItems(
+                *ItemStack.with(
+                    Items.graphite, 75,
+                    Items.silicon, 85,
+                    Items.titanium, 30,
+                    IonItems.zinc, 35
+                )
+            )
+            upgrades.add(arrayOf(IonUnitTypes.xender, IonUnitTypes.astro))
+
         }
         
     }
