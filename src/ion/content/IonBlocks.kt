ef181ec18f7a1e5aedd903647f14152f4d108c90
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-05 21:17:10 +0800

    hola

--- src/ion/content/IonBlocks.kt
+++ src/ion/content/IonBlocks.kt
@@ -121,7 +121,7 @@ object IonBlocks{
                 health = 350
                 size = 2
                 craftTime = 60f
-                outputItems = *ItemStack.with(IonItems.brass, 1)
+                outputItems = ItemStack(IonItems.brass, 1)
                 consumePower(1.4f)
                 consumeItems(
                     *ItemStack.with(
