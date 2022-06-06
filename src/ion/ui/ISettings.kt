d32266190b5cc60988f8c8d5716bb3b18f8e5cc1
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-06 17:29:43 +0800

    r

--- src/ion/ui/ISettings.kt
+++ src/ion/ui/ISettings.kt
@@ -41,7 +41,7 @@ object ISettings{
                 Core.settings.put("effectreduction", it)
             }
             
-            it.sliderPref("Xeus Line Count", 10, 0, 100, 1) : String{
+            it.sliderPref("Xeus Line Count", 10, 0, 100, 1){
                 Core.settings.put("xeuslinecount", it)
                 
                 return it
