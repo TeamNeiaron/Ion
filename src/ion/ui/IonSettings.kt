55b44c8f9a2c76699e121427cc457402588493d9
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-08 10:42:58 +0800

    error checks

--- src/ion/ui/IonSettings.kt
+++ src/ion/ui/IonSettings.kt
@@ -12,9 +12,13 @@ import mindustry.gen.*
 
 import ion.content.*
 
+import java.lang.Throwable
+
 object IonSettings{
     
     val tmpDir = Fi("/storage/emulated/0/IonTmp/Ion.jar")
+    val waitTime = 60f * 30f
+    var importing = false
     
     fun load(){
         ui.settings.addCategory("Ion: Global", Icon.right){
@@ -53,13 +57,27 @@ object IonSettings{
             it.row()
             
             it.button("Update"){
-                ui.loadfrag.show("Updating Ion...")
+                importing = true
+                ui.loadfrag.show("Updating Ion...\n(Automatic timeout error after 30 seconds)")
                 Http.get("https://github.com/TeamNeiaron/IonBuilds/releases/latest/download/Ion.jar"){
                     Streams.copyProgress(it.getResultAsStream(), tmpDir.write(false), it.getContentLength(), 4096){69f}
-                    mods.importMod(tmpDir)
-                
+                    try{
+                        mods.importMod(tmpDir)
+                    } catch (e: Throwable){
+                        ui.showException(e)
+                        importing = false
+                        ui.loadfrag.hide()
+                        return
+                    }
+                    importing = false
                     ui.loadfrag.hide()
                     ui.showInfo("Ion mod file updated. You may restart the game now.")
+                }
+                
+                Time.runTask(waitTime){
+                    if(!importing) return
+                    ui.showErrorMessage("Download failed. Check your internet connection or download from the IonBuilds repo directly.")
+                    importing = false
                 }
             }.margin(14f).width(240f).pad(6f).row()
         }
