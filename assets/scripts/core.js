a96c4bb0a53f0e4890e0d21fc2546225e6bf550c
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-06-29 18:05:32 +0800

    the

---
+++ assets/scripts/core.js
@@ -0,0 +1,16 @@
+function getInstance(name){
+    return Reflect.get(Vars.mods.getMod("ion").loader.loadClass(name), "INSTANCE")
+}
+
+const IonVars = getInstance("ion.IonVars")
+const IonUnitTypes = getInstance("ion.content.IonUnitTypes")
+const IonBlocks = getInstance("ion.content.IonBlocks")
+const IonStatusEffects = getInstance("ion.content.IonStatusEffects")
+const IonBullets = getInstance("ion.content.IonBullets")
+const IonFx = getInstance("ion.content.IonFx")
+const IonItems = getInstance("ion.content.IonItems")
+const IonTechTree = getInstance("ion.content.IonTechTree")
+const IColor = getInstance("ion.defs.IColor")
+const IDraw = getInstance("ion.defs.IDraw")
+const ISettings = getInstance("ion.ui.ISettings")
+const Utils = getInstance("ion.defs.Utils")
