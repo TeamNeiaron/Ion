1275cdbf632ce4738bb46089cca2b46ec9d06957
SMOLKEYS <jiroshimonis@gmail.com>
2022-07-06 15:17:48 +0800

    do not use those acursed tabs

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,8 @@ dependencies {
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_1_8
-	targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.register("jarAndroid") {
