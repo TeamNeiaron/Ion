e0fab772698d3069f74c750b1c4c3b5ef3e084de
SMOLKEYS <jiroshimonis@gmail.com>
2022-07-16 12:42:03 +0800

    no more of those two

--- src/ion/defs/IDraw.kt
+++ src/ion/defs/IDraw.kt
@@ -11,23 +11,5 @@ object IDraw{
     fun unit(unit: mindustry.gen.Unit){
         Draw.rect(unit.type.fullIcon, unit.x, unit.y, unit.hitSize * 1.5f, unit.hitSize * 1.5f, 270f + unit.rotation)
     }
-    
-    fun block(
-        block: TextureRegion,
-        x: Float,
-        y: Float,
-        shakeX: Float,
-        shakeY: Float
-    ){
-        Draw.rect(block, x - Mathf.random(shakeX, -shakeX), y + Mathf.random(shakeY, -shakeY))
-    }
-    
-    fun block(
-        block: TextureRegion,
-        anchor: Building,
-        shakeX: Float,
-        shakeY: Float
-    ){
-        block(block, anchor.x, anchor.y, shakeX, shakeY)
-    }
+
 }
